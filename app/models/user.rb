2c6d64a735424bf6bc5c48d0260142174a3b946e
He Limin <chelimin@gmail.com>
2016-03-22 16:50:05 +0800

    css changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :posts
   validates :email, presence: true, uniqueness: true
-  # validates :user_name, presence: true, uniqueness: true
+  validates :user_name, uniqueness: true
 end
